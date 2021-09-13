bc21b9b62c0011b02d6a31e200d4ec5a1c05a95d
Onur GURSOY <onurgursoy@mail.com>
2021-09-13 19:37:26 +0300

    initial commit

---
+++ src/main/java/com/muhendisbey/dockerjavaapiexample/DockerJavaApiExampleApplication.java
@@ -0,0 +1,15 @@
+package com.muhendisbey.dockerjavaapiexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJavaApiExampleApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DockerJavaApiExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muhendisbey/dockerjavaapiexample/DockerJavaApiExampleApplicationTests.java
@@ -0,0 +1,15 @@
+package com.muhendisbey.dockerjavaapiexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJavaApiExampleApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
